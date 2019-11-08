3636a90d1d5087cea136eb9236de096daff003b1
GlenVanPuyenbroeck <glen_vp@hotmail.com>
2019-11-08 15:49:09 +0100

    Update main.cpp
    Spatie toegevoegd als test

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 
     // rect foscusable maken
     player -> setFlag(QGraphicsItem::ItemIsFocusable);
-    player ->setFocus();
+    player -> setFocus();
 
     // Vieuw toevoegen
 
